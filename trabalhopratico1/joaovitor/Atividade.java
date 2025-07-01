f3e2a605bf58984fbe34527625f4716c31dd82da
JV608 <a2024952024@teiacoltec.org>
2025-07-01 16:16:38 -0300

    Add files via upload

---
+++ trabalhopratico1/joaovitor/Atividade.java
@@ -0,0 +1,41 @@
+package org.teiacoltec.poo.tp1;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+public class Atividade{
+    private int ID;
+    private String nome;
+    private String descricao;
+    private LocalDate inicio;
+    private LocalDate fim;
+    private static DateTimeFormatter modelo = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Atividade(int ID, String nome, String descricao, String inicio, String fim){
+        this.ID = ID;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.inicio = LocalDate.parse(inicio, modelo);
+        this.fim = LocalDate.parse(fim, modelo);
+    }
+    public int getID(){
+        return ID;
+    }
+    public String getnome(){
+        return nome;
+    }
+    public String getdescricao(){
+        return descricao;
+    }
+    public LocalDate getinicio(){
+        return inicio;
+    }
+    public LocalDate getfim(){
+        return fim;
+    }
+    public String obterInformacoes(){
+        String informacoes = "Informacoes da atividade "+getnome()+":\nID: "+ getID() + "\nDescrição: "+getdescricao()+"\nInicio da atividade: "+getinicio()+"\nFim do Prazo: "+getfim();
+        return informacoes;
+    }
+}
