10c27d25974f0b970af7f6783b18081a2ba9239b
JV608 <a2024952024@teiacoltec.org>
2025-07-01 16:21:48 -0300

    Create Aluno.java

---
+++ trabalhopratico1/Gabriel/Aluno.java
@@ -0,0 +1,34 @@
+package org.teiacoltec.poo.tp1;
+
+import java.time.LocalDate;
+
+public class Aluno extends Pessoa {
+    
+    private String Matricula;
+    private String Curso;
+    
+    Aluno(String CPF, String nome, LocalDate nascimento, String email, String endereco, String Matricula, String Curso){
+
+        super(CPF, nome, nascimento, email, endereco);
+        this.Matricula = Matricula;
+        this.Curso = Curso;
+
+    }
+
+    public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(String Matricula) {
+        this.Matricula = Matricula;
+    }
+
+    public String getCurso() {
+        return Curso;
+    }
+
+    public void setCurso(String Curso) {
+        this.Curso = Curso;
+    }
+
+}
